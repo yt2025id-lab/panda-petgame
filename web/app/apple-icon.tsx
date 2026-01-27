40818bbdfd2b955fe95cf2b870ffa833ed006d51
Ozan_OnChain <yt2025id@gmail.com>
2026-01-28 00:44:42 +0700

    feat: update favicon to panda emoji icon

---
+++ web/app/apple-icon.tsx
@@ -0,0 +1,34 @@
+import { ImageResponse } from 'next/og'
+
+export const runtime = 'edge'
+
+export const size = {
+  width: 180,
+  height: 180,
+}
+
+export const contentType = 'image/png'
+
+export default function AppleIcon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 120,
+          background: 'white',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          borderRadius: '20%',
+        }}
+      >
+        🐼
+      </div>
+    ),
+    {
+      ...size,
+    }
+  )
+}
